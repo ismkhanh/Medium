7f5c6c29688059166cbdd0576fb14b4476619811
Ismail Khan <ikglobal024@gmail.com>
2018-05-02 22:31:35 +0530

    Testable Code for Android

---
+++ app/src/main/java/com/ik/testablecode/ApiService.java
@@ -0,0 +1,17 @@
+package com.ik.testablecode;
+
+/**
+ * Created by ismailkhan on 01/05/18.
+ */
+
+public class ApiService {
+
+    public String getData() {
+        return "String from API";
+    }
+}
+
+
+
+
+
